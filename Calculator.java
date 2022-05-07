709a5ff09a0dee555e358b70ec475d4c0816df02
AndrianoVladislav <andrianovladislav13@gmail.com>
2022-05-07 19:01:50 +0200

    HW_final_fix_1

--- Calculator.java
+++ Calculator.java
@@ -1,32 +1,26 @@
 public class Calculator { 
     public static void main(String[] args) {
-        int variable1 = 6;
-        int variable2 = 4;
-        char symbolPlus = '\u002B';
-        char symbolMinus = '\u002D';
-        char symbolMultiply = '\u002A';
-        char symbolDivide  = '\u002F';
-        char symbol5 = '\u0025';
-        char symbol6 = '\u005E';
-        int sign = symbol6;
-        if (sign == symbolPlus) {
-            System.out.println(variable1 + " " + symbolPlus + " " + variable2 + " = " + (variable1 + variable2));
-        } else if (sign == symbolMinus) {
-            System.out.println(variable1 + " " + symbolMinus + " " + variable2 + " = " + (variable1 - variable2));
-        } else if (sign == symbolMultiply) {
-            System.out.println(variable1 + " " + symbolMultiply + " " + variable2 + " = " + (variable1 * variable2));
-        } else if (sign == symbolDivide ) {
-            float variable11 = (float) variable1;
-            float variable21 = (float) variable2;
-            System.out.format(variable1 + " " + symbolDivide  + " " + variable2 + " = " + "%1$-10.2f", (variable11 / variable21));
-        } else if (sign == symbol5) {
-            System.out.println(variable1 + " " + symbol5 + " " + variable2 + " = " + (variable1 % variable2));
-        } else if (sign == symbol6) {
-            int result = 1;
-            for (int i = 1; i <= variable2; i++) {
-                result *= variable1;
+        double num1 = 6.5;
+        double num2 = 4.5;
+        double result = 0;
+        char symbol = '^';
+        int sign = symbol;
+        if (sign == '+') {
+            result = num1 + num2;
+        } else if (sign == '-') {
+            result = num1 - num2;
+        } else if (sign == '*') {
+            result = num1 * num2;
+        } else if (sign == '/' ) {
+            result = num1 / num2;
+        } else if (sign == '%') {
+            result = num1 % num2;
+        } else if (sign == '^') {
+            result += 1;
+            for (int i = 1; i <= num2; i++) {
+                result *= num1;
             }
-            System.out.println(variable1 + " " + symbol6 + " " + variable2 + " = " + result);
         }
+        System.out.format(num1 + " " + symbol + " " + num2 + " = " + "%1$-10.2f", result);
     }
 }
