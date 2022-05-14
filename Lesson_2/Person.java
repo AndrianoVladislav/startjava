a2a62e12fd3eb7a21ce164c75a018708677b1fac
AndrianoVladislav <andrianovladislav13@gmail.com>
2022-05-14 11:08:57 +0200

    HW2_Fix

--- Lesson_2/Person.java
+++ Lesson_2/Person.java
@@ -1,21 +1,27 @@
 public class Person {
+
     String gender = "Мужской";
     String name = "Вова";
     float height = 181.6f;
     float weight = 60.4f;
     int age = 25;
+
     void go() {
         System.out.println("Идёт");
     }
+
     void sit() {
         System.out.println("Сидит");
     }
+
     void run() {
         System.out.println("Бежит");
     }
+
     String talk() {
         return "Привет";
     }
+
     boolean learnJava() {
         return true;
     }
--- Lesson_2/Wolf.java
+++ Lesson_2/Wolf.java
@@ -1,22 +1,27 @@
 public class Wolf {
+
     String gender;
     String name;
     float height;
     int age;
     String color;
-    
+
     void go() {
         System.out.println("Идёт");
     }
+
     void sit() {
         System.out.println("Сидит");
     }
+
     void run() {
         System.out.println("Бежит");
     }
+
     void howls() {
         System.out.println("Воет");
     }
+
     void hunt() {
         System.out.println("Охотиться");
     }
